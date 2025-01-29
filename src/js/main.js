f7bba2bb122e6fc2b845d9bac849ec4dae5b83ff
Frida Jansson <frja2400@student.miun.se>
2025-01-29 16:31:06 +0100

    Adderade tabell till min HTML-kod och anropade json-filen med fetch. Sedan skapade jag en asynkron funktion som läser in datan och skriver ut den till skärmen.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,36 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Boilerplate är laddad och klar!');
-});
+async function fetchData() {
+    try {
+        const response = await fetch('https://webbutveckling.miun.se/files/ramschema_ht24.json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
+    }
+}
+
+async function processData() {
+    try {
+        const result = await fetchData();
+
+        const tableBody = document.getElementById('tableBody');
+        tableBody.innerHTML = '';
+
+        result.forEach (item => {
+            const newRow = `
+            <tr>
+            <td>${item.code}</td>
+            <td>${item.coursename}</td>
+            <td>${item.progression}</td>
+            </tr>
+            `;
+            tableBody.innerHTML += newRow;
+        });
+    } catch (error) {
+        console.error('Process error:', error);
+    }
+}
+
+processData();
